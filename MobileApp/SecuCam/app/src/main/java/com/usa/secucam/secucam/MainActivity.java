39c0a42b3f2466aa816f379c0034b89e253ec774
Maulik Soni <soni.maulik@gmail.com>
2015-01-01 19:19:58 -0600

    update

--- MobileApp/SecuCam/app/src/main/java/com/usa/secucam/secucam/MainActivity.java
+++ MobileApp/SecuCam/app/src/main/java/com/usa/secucam/secucam/MainActivity.java
@@ -1,17 +1,82 @@
 package com.usa.secucam.secucam;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.net.Uri;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
+/*implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener*/
+
+    /*private MediaPlayer mediaPlayer;
+    private SurfaceHolder vidHolder;
+    private SurfaceView vidSurface;
+    String vidAddress = "https://sccam.blob.core.windows.net/videos/a.mp4";*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+        String vidAddress = "http://sccam.blob.core.windows.net/videos/a34d213c-909f-11e4-9876-0013efb036d6.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+
+        try {
+            /*
+            // Creating HTTP client
+            HttpClient httpClient = new DefaultHttpClient();
+            // Creating HTTP Post
+            HttpPost httpPost = new HttpPost("https://sccam.blob.core.windows.net/videos?restype=container&comp=list");
+            // Making HTTP Request
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                // writing response to log
+                Log.d("Http Response:", response.toString());
+            } catch (ClientProtocolException e) {
+                // writing exception to log
+                e.printStackTrace();
+            } catch (IOException e) {
+                // writing exception to log
+                e.printStackTrace();
+            }
+            */
+            vidView.start();
+        }catch (Exception e){
+            Log.e("t",e.toString());
+        }
+        /*
+        vidSurface = (SurfaceView) findViewById(R.id.surfView);
+        vidHolder = vidSurface.getHolder();
+        vidHolder.addCallback(this);
+        */
+
     }
 
 
@@ -36,4 +101,34 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+/*
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(vidHolder);
+            mediaPlayer.setDataSource(vidAddress);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        //start playback
+        mediaPlayer.start();
+    }*/
 }
